ebdba55c5ffcfd29b47e3b5790dd10943274f73b
bender <zach.bender@protonmail.com>
2019-09-30 15:54:31 -0600

    build msg enhance; new get_msg_model implementation;

--- alpaka/configurator.py
+++ alpaka/configurator.py
@@ -26,32 +26,55 @@ class Configurator():
         return self.cfg.get(key, default)
 
 
+    def get_msg_model(self, cmd) -> tuple:
+        """
+         Exctract a message model extracted from config's CMD_MODEL property.
+        (It is probably will be assigned to the KernelMsgModel.nla_map somewhere)
+
+        @param cmd: <int or str> model name to extract from CMD_MODEL. For int
+                    value, model will be extracted directly from CMD_MODEL.
+                    For Str value, it will look up CMD_OPTS for that index.
+
+        @return: a model defined for the cmd in the config for the CMD_MODEL.
+        """
+
+        #Extracting a correlated Index from the CMD_OPTS for the str cmd
+        if isinstance(cmd, str):
+            if cmd not in self.cmd_opts:
+                msg = 'get_msg_model() -> cmd=%s not in cmd_opts: %s'
+                msg = msg % (cmd, list(self.cmd_opts.keys))
+                logging.warning(msg)
+            else:
+                cmd = self.cmd_opts.get(cmd)
+        #cmd is an int - so look up CMD_MODEL directly
+        elif cmd not in self.cmd_model:
+            msg = 'get_msg_model() -> cmd=%s not found in CMD_MODEL keys: %s'
+            msg = msg % (cmd, list(self.cmd_model.keys()))
+            logging.warning(msg)
+
+        return self.cmd_model.get(cmd, None)
+
+
     @property
     def version(self) -> int:
         return self.get('VERSION', 1)
+
 
     @property
     def family_name(self) -> str:
         return self.get('NETLINK_FAMILY_NAME', None)
 
-    @property
-    def msg_model(self) -> tuple:
-        return self.cfg['MSG_MODEL']
 
     @property
     def cmd_opts(self) -> dict:
         return self.cfg['CMD_OPTS']
 
+
     @property
     def cmd_model(self) -> dict:
         return self.get('CMD_MODEL', None)
 
-    @cmd_model.setter
-    def cmd_model(self, value):
-        if not isinstance(value, dict):
-            msg = 'Unexpected value type when cetting cmd_model! '
-            msg += 'Expected <dict> but %s has arrived!' % type(value)
-            logging.error(msg)
-            return
 
+    @cmd_model.setter
+    def cmd_model(self, value: tuple):
         self.cfg['CMD_MODEL'] = value
